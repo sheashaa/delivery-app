03b041bae0c99b5d3d004ee14ddc6db22d00dfc2
not zakister <37934900+sheashaa@users.noreply.github.com>
2021-05-21 04:11:16 +0200

    More reorganization. Added error page.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,58 +4,39 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AuthorizationModule } from './shared/authorization/authorization.module';
 import { AuthorizationInterceptor } from './shared/authorization/authorization.interceptor';
+import { HomeModule } from './home/home.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './home/components/about/about.component';
-import { WhyUsComponent } from './home/components/why-us/why-us.component';
-import { MenuComponent } from './home/components/menu/menu.component';
-import { SpecialsComponent } from './home/components/specials/specials.component';
-import { EventsComponent } from './home/components/events/events.component';
-import { BookATableComponent } from './home/components/book-a-table/book-a-table.component';
-import { TestimonialsComponent } from './home/components/testimonials/testimonials.component';
-import { GalleryComponent } from './home/components/gallery/gallery.component';
-import { ChefsComponent } from './home/components/chefs/chefs.component';
-import { ContactComponent } from './home/components/contact/contact.component';
-import { HeroComponent } from './home/components/hero/hero.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HeroComponent,
-    AboutComponent,
-    WhyUsComponent,
-    MenuComponent,
-    SpecialsComponent,
-    EventsComponent,
-    BookATableComponent,
-    TestimonialsComponent,
-    GalleryComponent,
-    ChefsComponent,
-    ContactComponent,
-    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    AuthorizationModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '**', component: NotFoundComponent },
     ]),
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       maxOpened: 5
     }),
+    AuthorizationModule,
+    HomeModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizationInterceptor, multi: true }
---
+++ ClientApp/src/app/home/home.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SpecialsComponent } from './components/specials/specials.component';
+import { EventsComponent } from './components/events/events.component';
+import { BookATableComponent } from './components/book-a-table/book-a-table.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ChefsComponent } from './components/chefs/chefs.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+    ]),
+  ],
+  declarations: [
+    HeroComponent,
+    AboutComponent,
+    WhyUsComponent,
+    MenuComponent,
+    SpecialsComponent,
+    EventsComponent,
+    BookATableComponent,
+    TestimonialsComponent,
+    GalleryComponent,
+    ChefsComponent,
+    ContactComponent,
+    HomeComponent,
+  ],
+  exports: [HomeComponent]
+})
+export class HomeModule { }
